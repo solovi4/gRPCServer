6266455ba992e2aed7ed6f94f424e0b8020fb6d1
solovi4 <rymbil@mail.ru>
2021-11-08 22:32:45 +0500

    Add Server gPRC

---
+++ gPRCService/GrpcService/Protos/RandomizerService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace GrpcService.Protos
+{
+    public class RandomizerService : Randomizer.RandomizerBase
+    {
+        private Random _random;
+        public RandomizerService()
+        {
+            _random = new Random();
+        }
+        public override Task<RandomResponse> GetRandom(RandomRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new RandomResponse {Numb = _random.Next()});
+        }
+    }
+}
